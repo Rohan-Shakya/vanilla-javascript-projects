fca0f2eb35f119908e5c22a94fc481ddd882b73d
Rohan-shakya <rohanshakya254@gmail.com>
2020-09-11 22:55:50 +0545

    added ui for demos and source code

--- datas.js
+++ datas.js
@@ -1,8 +1,202 @@
 export const datas = [
   {
     id: 1,
-    title: 'Github',
-    sourceCode: 'https://github.com',
-    demo: 'https://github.com',
+    title: 'Lorem Ipsum Generator',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/lorem-ipsum-generator',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/lorem-ipsum-generator',
+  },
+  {
+    id: 2,
+    title: 'Temperature Convertor',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/temperature-convertor',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/temperature-convertor',
+  },
+  {
+    id: 3,
+    title: 'Ludo Game',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/ludo-game',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/ludo-game',
+  },
+  {
+    id: 4,
+    title: 'Weight Convertor',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/weight-converter',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/weight-converter',
+  },
+  {
+    id: 5,
+    title: 'Full Image Slider',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/full-image-slider',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/full-image-slider',
+  },
+  {
+    id: 6,
+    title: 'Drag And Drop',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/drag-and-drop',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/drag-and-drop',
+  },
+  {
+    id: 7,
+    title: 'Snow Effect',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/snow-effect',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/snow-effect',
+  },
+  {
+    id: 8,
+    title: 'Smooth Scrolling',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/smooth-scrolling',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/smooth-scrolling',
+  },
+  {
+    id: 9,
+    title: 'Site Bookmarker',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/site-bookmark',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/site-bookmark',
+  },
+  {
+    id: 10,
+    title: 'Todo List',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/todo-list',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/todo-list',
+  },
+  {
+    id: 11,
+    title: 'Rock Paper Scissor',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/rock-paper-scissor',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/rock-paper-scissor',
+  },
+  {
+    id: 12,
+    title: 'Form Validation',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/form-validation',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/form-validation',
+  },
+  {
+    id: 13,
+    title: 'Password Generator',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/passoword-generator',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/passoword-generator',
+  },
+  {
+    id: 14,
+    title: 'Filterable Menu',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/filterable-menu',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/filterable-menu',
+  },
+  {
+    id: 15,
+    title: 'Memory Game',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/memory-game',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/memory-game',
+  },
+  {
+    id: 16,
+    title: 'Calculator',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/calculator',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/calculator',
+  },
+  {
+    id: 17,
+    title: 'Quiz Game',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/quiz-game',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/quiz-game',
+  },
+  {
+    id: 18,
+    title: 'Drawing App',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/drawing-app',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/drawing-app',
+  },
+  {
+    id: 19,
+    title: 'Corona Tracker',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/corona-tracker',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/corona-tracker',
+  },
+  {
+    id: 20,
+    title: 'Weather App',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/weather-app',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/weather-app',
+  },
+  {
+    id: 21,
+    title: 'Movie App',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/movie-app',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/movie-app',
+  },
+  {
+    id: 22,
+    title: 'Currency Convertor',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/currency-convertor',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/currency-convertor',
+  },
+  {
+    id: 23,
+    title: 'Tic Tac Toe',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/tic-tac-toe',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/tic-tac-toe',
+  },
+  {
+    id: 24,
+    title: 'Snake Game',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/snake-game',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/snake-game',
+  },
+  {
+    id: 25,
+    title: 'Github Profile Finder',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/vanilla-javascript-projects/tree/master/github-profile-finder',
+    demo:
+      'https://rohan-shakya.github.io/vanilla-javascript-projects/github-profile-finder',
   },
 ];
